a315f9e30a479ad75ee0e0bb829998fe0ba8a821
saurabhsathe <sathesaurabh30@gmail.com>
2022-05-15 19:14:48 -0800

    added the failure conditions

--- healthchek/client.py
+++ healthchek/client.py
@@ -3,8 +3,20 @@ import grpc
 import healthcheck_pb2
 import healthcheck_pb2_grpc
 
+
 channel=grpc.insecure_channel('localhost:50051')
 req = healthcheck_pb2.healthCheckRequest()
 stub=healthcheck_pb2_grpc.SentinelMonitoringStub(channel)
-response=stub.healthCheck(req)
-print("received reponse",response.status)
+failed_attempts=0
+while True:
+
+    try:
+        response=stub.healthCheck(req,timeout = 3)
+        print("successful response ",response.status)
+    except Exception as e:
+        failed_attempts+=1
+        print("Process failed")
+        if failed_attempts==3:
+            print("Failed far too many times. Convery gang leader")
+            failed_attempts=0
+            break
